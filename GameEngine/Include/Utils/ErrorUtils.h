427a9a5b1f8b69db6fb46fe5ca2234a871e2a9ab
CHOIJIONE\choij <choijione02@gmail.com>
2024-02-05 17:54:51 +0900

    프로젝트A의 어선셜 매크로 내 기능들을 하나의 ErrorUtils 네임 스페이스로 묶어서 정리
    - 디버그 창에 출력하는 기능의 함수 이름 수정(AssertPrintF ⇒ DebugPrintF)
    - OpenGL 에러 열거형은 단순 상수이므로, 부호 없는 정수형으로 에러 코드를 받도록 변경

---
+++ GameEngine/Include/Utils/ErrorUtils.h
@@ -0,0 +1,83 @@
+#pragma once
+
+#include <string>
+
+
+/**
+ * @brief 에러에 대한 확장 기능을 제공합니다.
+ */
+namespace ErrorUtils
+{
+	/**
+	 * @brief 디버그 창에 형식화된 문자열을 출력합니다.
+	 *
+	 * @param format 형식 문자열입니다.
+	 * @param .. 문자열 내의 형식에 대응하는 가변인자입니다.
+	 *
+	 * @note
+	 * - Debug와 Release모드에서만 디버그 창에 문자열이 출력됩니다.
+	 * - Shipping 모드에서는 디버그 창에 문자열이 출력되지 않습니다.
+	 */
+	void DebugPrintF(const char* format, ...);
+
+
+	/**
+	 * @brief 디버그 창에 형식화된 문자열을 출력합니다.
+	 *
+	 * @param format 형식 문자열입니다.
+	 * @param .. 문자열 내의 형식에 대응하는 가변인자입니다.
+	 *
+	 * @note
+	 * - Debug와 Release모드에서만 디버그 창에 문자열이 출력됩니다.
+	 * - Shipping 모드에서는 디버그 창에 문자열이 출력되지 않습니다.
+	 */
+	void DebugPrintF(const wchar_t* format, ...);
+
+
+	/**
+	 * @brief 에러 코드에 대응하는 메시지를 얻습니다.
+	 *
+	 * @param errorCode 확인할 에러 코드입니다.
+	 *
+	 * @return 에러 코드에 대응하는 메시지를 반환합니다.
+	 */
+	std::string GetWindowsErrorMessageA(const uint32_t& errorCode);
+
+
+	/**
+	 * @brief 에러 코드에 대응하는 메시지를 얻습니다.
+	 *
+	 * @param errorCode 확인할 에러 코드입니다.
+	 *
+	 * @return 에러 코드에 대응하는 메시지를 반환합니다.
+	 */
+	std::wstring GetWindowsErrorMessageW(const uint32_t& errorCode);
+
+
+	/**
+	 * @brief 에러 코드에 대응하는 메시지를 얻습니다.
+	 *
+	 * @param errorCode 확인할 에러 코드입니다.
+	 *
+	 * @return 에러 코드에 대응하는 메시지를 반환합니다.
+	 *
+	 * @note
+	 * - https://registry.khronos.org/OpenGL-Refpages/gl4/html/glGetError.xhtml
+	 * - https://www.khronos.org/opengl/wiki/OpenGL_Error
+	 */
+	std::string GetGLErrorMessageA(const uint32_t& errorCode);
+
+
+	/**
+	 * @brief 에러 코드에 대응하는 메시지를 얻습니다.
+	 *
+	 * @param errorCode 확인할 에러 코드입니다.
+	 *
+	 * @return 에러 코드에 대응하는 메시지를 반환합니다.
+	 *
+	 * @note
+	 * - https://registry.khronos.org/OpenGL-Refpages/gl4/html/glGetError.xhtml
+	 * - https://www.khronos.org/opengl/wiki/OpenGL_Error
+	 */
+	std::wstring GetGLErrorMessageW(const uint32_t& errorCode);
+}
---
+++ GameEngine/Source/Utils/ErrorUtils.cpp
@@ -0,0 +1,160 @@
+#include "Utils/ErrorUtils.h"
+
+#include <cstdarg>
+#include <cstdio>
+#include <windows.h>
+
+#include <glad/glad_wgl.h>
+#include <glad/glad.h>
+
+const int32_t BUFFER_SIZE = 1024;
+char charBuffer[BUFFER_SIZE];
+wchar_t wcharBuffer[BUFFER_SIZE];
+
+void ErrorUtils::DebugPrintF(const char* format, ...)
+{
+	va_list args;
+	va_start(args, format);
+	int32_t size = _vsnprintf_s(charBuffer, BUFFER_SIZE, format, args);
+	va_end(args);
+
+	OutputDebugStringA(charBuffer);
+}
+
+void ErrorUtils::DebugPrintF(const wchar_t* format, ...)
+{
+	va_list args;
+	va_start(args, format);
+	int32_t size = _vsnwprintf_s(wcharBuffer, BUFFER_SIZE, format, args);
+	va_end(args);
+
+	OutputDebugStringW(wcharBuffer);
+}
+
+std::string ErrorUtils::GetWindowsErrorMessageA(const uint32_t& errorCode)
+{
+	uint32_t size = FormatMessageA(
+		FORMAT_MESSAGE_FROM_SYSTEM,
+		nullptr,
+		static_cast<DWORD>(errorCode),
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		charBuffer,
+		BUFFER_SIZE,
+		nullptr
+	);
+
+	return std::string(charBuffer, size);
+}
+
+std::wstring ErrorUtils::GetWindowsErrorMessageW(const uint32_t& errorCode)
+{
+	uint32_t size = FormatMessageW(
+		FORMAT_MESSAGE_FROM_SYSTEM,
+		nullptr,
+		static_cast<DWORD>(errorCode),
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		wcharBuffer,
+		BUFFER_SIZE,
+		nullptr
+	);
+
+	return std::wstring(wcharBuffer, size);
+}
+
+std::string ErrorUtils::GetGLErrorMessageA(const uint32_t& errorCode)
+{
+	std::string errorMessage;
+
+	switch (errorCode)
+	{
+	case GL_NO_ERROR:
+		errorMessage = "No error has been recorded. (GL_NO_ERROR)";
+		break;
+
+	case GL_INVALID_ENUM:
+		errorMessage = "An unacceptable value is specified for an enumerated argument. (GL_INVALID_ENUM)";
+		break;
+
+	case GL_INVALID_VALUE:
+		errorMessage = "A numeric argument is out of range. (GL_INVALID_VALUE)";
+		break;
+
+	case GL_INVALID_OPERATION:
+		errorMessage = "The specified operation is not allowed in the current state. (GL_INVALID_OPERATION)";
+		break;
+
+	case GL_INVALID_FRAMEBUFFER_OPERATION:
+		errorMessage = "The framebuffer object is not complete. (GL_INVALID_FRAMEBUFFER_OPERATION)";
+		break;
+
+	case GL_OUT_OF_MEMORY:
+		errorMessage = "There is not enough memory left to execute the command. (GL_OUT_OF_MEMORY)";
+		break;
+
+	case GL_STACK_UNDERFLOW:
+		errorMessage = "An attempt has been made to perform an operation that would cause an internal stack to underflow. (GL_STACK_UNDERFLOW)";
+		break;
+
+	case GL_STACK_OVERFLOW:
+		errorMessage = "An attempt has been made to perform an operation that would cause an internal stack to overflow. (GL_STACK_OVERFLOW)";
+		break;
+
+	case GL_CONTEXT_LOST:
+		errorMessage = "This occurs when the OpenGL context becomes lost due to a graphics card reset. (GL_CONTEXT_LOST)";
+		break;
+
+	default:
+		errorMessage = "Undefined error message.";
+	}
+
+	return errorMessage;
+}
+
+std::wstring ErrorUtils::GetGLErrorMessageW(const uint32_t& errorCode)
+{
+	std::wstring errorMessage;
+
+	switch (errorCode)
+	{
+	case GL_NO_ERROR:
+		errorMessage = L"No error has been recorded. (GL_NO_ERROR)";
+		break;
+
+	case GL_INVALID_ENUM:
+		errorMessage = L"An unacceptable value is specified for an enumerated argument. (GL_INVALID_ENUM)";
+		break;
+
+	case GL_INVALID_VALUE:
+		errorMessage = L"A numeric argument is out of range. (GL_INVALID_VALUE)";
+		break;
+
+	case GL_INVALID_OPERATION:
+		errorMessage = L"The specified operation is not allowed in the current state. (GL_INVALID_OPERATION)";
+		break;
+
+	case GL_INVALID_FRAMEBUFFER_OPERATION:
+		errorMessage = L"The framebuffer object is not complete. (GL_INVALID_FRAMEBUFFER_OPERATION)";
+		break;
+
+	case GL_OUT_OF_MEMORY:
+		errorMessage = L"There is not enough memory left to execute the command. (GL_OUT_OF_MEMORY)";
+		break;
+
+	case GL_STACK_UNDERFLOW:
+		errorMessage = L"An attempt has been made to perform an operation that would cause an internal stack to underflow. (GL_STACK_UNDERFLOW)";
+		break;
+
+	case GL_STACK_OVERFLOW:
+		errorMessage = L"An attempt has been made to perform an operation that would cause an internal stack to overflow. (GL_STACK_OVERFLOW)";
+		break;
+
+	case GL_CONTEXT_LOST:
+		errorMessage = L"This occurs when the OpenGL context becomes lost due to a graphics card reset. (GL_CONTEXT_LOST)";
+		break;
+
+	default:
+		errorMessage = L"Undefined error message.";
+	}
+
+	return errorMessage;
+}
