e30e4f0399cfa864bbe6fd00c3cc19be805a5fbd
DESKTOP-CIQEUKA\choij <choijione02@gmail.com>
2024-02-06 08:31:23 +0900

    입력 처리 매니저에 누락된 헤더 선언 추가

--- GameEngine/Source/Core/InputManager.cpp
+++ GameEngine/Source/Core/InputManager.cpp
@@ -5,6 +5,7 @@
 #include "Core/Window.h"
 
 #include <imgui.h>
+#include <imgui_impl_opengl3.h>
 #include <imgui_impl_win32.h>
 extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
