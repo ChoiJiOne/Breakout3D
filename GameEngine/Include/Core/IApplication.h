47342b26a16cb1e1b3f508181adf2eba63444139
DESKTOP-CIQEUKA\choij <choijione02@gmail.com>
2024-02-06 09:18:18 +0900

    프로젝트A의 어플리케이션 인터페이스 추가

---
+++ GameEngine/Include/Core/IApplication.h
@@ -0,0 +1,219 @@
+#pragma once
+
+#include "Core/AudioManager.h"
+#include "Core/GameTimer.h"
+#include "Core/IManager.h"
+#include "Core/InputManager.h"
+#include "Core/ObjectManager.h"
+#include "Core/RenderManager.h"
+#include "Core/ResourceManager.h"
+#include "Core/Vertex.h"
+#include "Core/Window.h"
+
+#include "Macro/Assertion.h"
+#include "Macro/GLAssertion.h"
+#include "Macro/Macro.h"
+#include "Macro/WindowsAssertion.h"
+
+#include "Matrix/Matrix2x2.h"
+#include "Matrix/Matrix3x3.h"
+#include "Matrix/Matrix4x4.h"
+
+#include "Object/Camera3D.h"
+#include "Object/IObject.h"
+
+#include "Resource/IResource.h"
+#include "Resource/Sound.h"
+#include "Resource/TTFont.h"
+
+#include "Shader/Shader.h"
+
+#include "Utils/CommandLineUtils.h"
+#include "Utils/ErrorUtils.h"
+#include "Utils/FileUtils.h"
+#include "Utils/GeometryGenerateUtils.h"
+#include "Utils/MathUtils.h"
+#include "Utils/StringUtils.h"
+#include "Utils/WindowClassUtils.h"
+#include "Utils/WindowsCrashUtils.h"
+
+
+/**
+ * @brief 애플리케이션 인터페이스입니다.
+ *
+ * @note 애플리케이션을 초기화 및 실행하는 클래스는 반드시 이 클래스를 상속받아야 합니다.
+ */
+class IApplication
+{
+public:
+	/**
+	 * @brief 애플리케이션 인터페이스의 생성자입니다.
+	 */
+	IApplication();
+
+
+	/**
+	 * @brief 애플리케이션 인터페이스의 가상 소멸자입니다.
+	 */
+	virtual ~IApplication();
+
+
+	/**
+	 * @brief 애플리케이션 인터페이스의 복사 생성자 및 대입 연산자를 명시적으로 삭제합니다.
+	 */
+	DISALLOW_COPY_AND_ASSIGN(IApplication);
+
+
+	/**
+	 * @brief 애플리케이션 인터페이스를 초기화합니다.
+	 *
+	 * @note 하위 클래스에서는 초기화 전에 이 메서드를 반드시 호출해야 합니다.
+	 */
+	virtual void Setup();
+
+
+	/**
+	 * @brief 애플리케이션 인터페이스의 사용을 종료합니다.
+	 */
+	virtual void Shutdown();
+
+
+	/**
+	 * @brief 애플리케이션을 실행합니다.
+	 */
+	virtual void Run() = 0;
+
+
+	/**
+	 * @brief 애플리케이션의 루트 경로를 얻습니다.
+	 *
+	 * @return 애플리케이션의 루트 경로를 반환합니다.
+	 */
+	static const std::wstring& GetRootPath() { return rootPath_; }
+
+
+	/**
+	 * @brief 애플리케이션의 엔진 경로를 얻습니다.
+	 *
+	 * @return 애플리케이션의 엔진 경로를 반환합니다.
+	 */
+	static const std::wstring& GetEnginePath() { return enginePath_; }
+
+
+protected:
+	/**
+	 * @brief 애플리케이션의 속성을 설정합니다.
+	 *
+	 * @param windowTitle 애플리케이션의 윈도우 타이틀입니다.
+	 * @param windowPosX 애플리케이션의 화면 상 윈도우의 X 위치입니다.
+	 * @param windowPosY 애플리케이션의 화면 상 윈도우의 X 위치입니다.
+	 * @param windowWidth 애플리케이션의 윈도우 가로 크기입니다.
+	 * @param windowHeight 애플리케이션의 윈도우 세로 크기입니다.
+	 * @param bIsResize 애플리케이션의 윈도우 크기 변경 여부입니다.
+	 * @param bIsFullscreen 애플리케이션의 윈도우 풀 스크린 여부입니다.
+	 * @param bIsVsync 애플리케이션의 수직 동기화 여부입니다.
+	 * @param bIsImGui ImGui 라이브러리 사용 여부입니다.
+	 */
+	void SetProperties(
+		const std::wstring& windowTitle,
+		int32_t windowPosX,
+		int32_t windowPosY,
+		int32_t windowWidth,
+		int32_t windowHeight,
+		bool bIsResize,
+		bool bIsFullscreen,
+		bool bIsVsync,
+		bool bIsImGui
+	);
+
+
+protected:
+	/**
+	 * @brief 게임 프레임워크를 초기화했는지 확인합니다.
+	 */
+	bool bIsSetup_ = false;
+
+
+	/**
+	 * @brief 속성 설정에 성공했는지 확인합니다.
+	 */
+	bool bIsPropertiesSet_ = false;
+
+
+	/**
+	 * @brief 개발 모드인지 확인합니다.
+	 */
+	bool bIsDevMode_ = false;
+
+
+	/**
+	 * @brief 애플리케이션 윈도우 타이틀입니다.
+	 */
+	std::wstring windowTitle_;
+
+
+	/**
+	 * @brief 애플리케이션의 화면 상 (x, y) 좌표입니다.
+	 */
+	Vector2i windowPosition_;
+
+
+	/**
+	 * @brief 애플리케이션의 화면 상의 가로 크기입니다.
+	 */
+	int32_t windowWidth_ = 0;
+
+
+	/**
+	 * @brief 애플리케이션의 화면 상의 세로 크기입니다.
+	 */
+	int32_t windowHeight_ = 0;
+
+
+	/**
+	 * @brief 애플리케이션의 윈도우 크기 변경 여부입니다.
+	 */
+	bool bIsResize_ = false;
+
+
+	/**
+	 * @brief 애플리케이션의 윈도우 풀 스크린 여부입니다.
+	 */
+	bool bIsFullscreen_ = false;
+
+
+	/**
+	 * @brief 애플리케이션의 수직 동기화 여부입니다.
+	 */
+	bool bIsVsync_ = true;
+
+
+	/**
+	 * @brief ImGui 라이브러리 사용 여부입니다.
+	 */
+	bool bIsImGui_ = false;
+
+
+	/**
+	 * @brief 게임 루프 종료가 감지되었는지 확인합니다.
+	 */
+	bool bIsDoneLoop_ = false;
+
+
+	/**
+	 * @brief 루트 경로입니다.
+	 */
+	static std::wstring rootPath_;
+
+
+	/**
+	 * @brief 게임 엔진 경로입니다.
+	 */
+	static std::wstring enginePath_;
+
+
+	/**
+	 * @brief 게임 윈도우입니다.
+	 */
+	std::unique_ptr<Window> window_ = nullptr;
+};
---
+++ GameEngine/Source/Core/IApplication.cpp
@@ -0,0 +1,142 @@
+#include "Core/IApplication.h"
+
+std::wstring IApplication::rootPath_;
+std::wstring IApplication::enginePath_;
+
+IApplication::IApplication()
+{
+	if (!bIsSetup_)
+	{
+		WindowsCrashUtils::RegisterWindowsExceptionFilter();
+	}
+}
+
+IApplication::~IApplication()
+{
+	if (bIsSetup_)
+	{
+		Shutdown();
+	}
+}
+
+void IApplication::Setup()
+{
+#if	defined(MIN_SIZE_REL_MODE)
+	if (bIsSetup_)
+	{
+		MessageBoxW(nullptr, L"Already setup application...", L"Error", MB_OK);
+		return;
+	}
+
+	if (!bIsPropertiesSet_)
+	{
+		MessageBoxW(nullptr, L"Failed to set application properties...", L"Error", MB_OK);
+		return;
+	}
+#else
+	ASSERT(!bIsSetup_, "already setup application...");
+	ASSERT(bIsPropertiesSet_, "Failed to set application properties...");
+#endif
+
+	CommandLineUtils::Parse();
+
+	std::wstring devMode;
+	if (CommandLineUtils::GetStringValue("devMode", devMode))
+	{
+		devMode = StringUtils::ToLower(devMode);
+		bIsDevMode_ = (devMode == L"on") ? true : false;
+	}
+
+	if (bIsDevMode_)
+	{
+		ASSERT(CommandLineUtils::GetStringValue(L"rootPath", rootPath_), "failed to get root path from command line...");
+	}
+	else
+	{
+		rootPath_ = FileUtils::GetBasePath(CommandLineUtils::GetExecutePath());
+	}
+
+	enginePath_ = rootPath_ + L"GameEngine/";
+
+	WindowClassUtils::RegisterWindowClass(windowTitle_, InputManager::WindowProc);
+
+	window_ = std::make_unique<Window>();
+	window_->Create(Window::WindowConstructParam{
+		windowTitle_, windowPosition_.x, windowPosition_.y, windowWidth_, windowHeight_,
+		bIsResize_, bIsFullscreen_
+		});
+
+	InputManager::Get().SetInputControlWindow(window_.get());
+	InputManager::Get().SetEnableImGui(bIsImGui_);
+
+	RenderManager::Get().SetRenderTargetWindow(window_.get());
+	RenderManager::Get().SetEnableImGui(bIsImGui_);
+
+	InputManager::Get().Startup();
+	AudioManager::Get().Startup();
+	ResourceManager::Get().Startup();
+	RenderManager::Get().Startup();
+	ObjectManager::Get().Startup();
+
+	auto defaultLoopDoneEvent = [&]()
+	{
+		bIsDoneLoop_ = true;
+	};
+
+	auto defaultResizeEvent = [&]()
+	{
+		window_->GetSize(windowWidth_, windowHeight_);
+		RenderManager::Get().Resize();
+	};
+
+	auto defaultMoveEvent = [&]()
+	{
+		window_->GetPosition(windowPosition_.x, windowPosition_.y);
+	};
+
+	InputManager::Get().AddWindowEventAction("DefaultLoopDoneEvent", EWindowEvent::Close, defaultLoopDoneEvent, true);
+	InputManager::Get().AddWindowEventAction("DefaultMoveEvent", EWindowEvent::Move, defaultMoveEvent, true);
+	InputManager::Get().AddWindowEventAction("DefaultResizeEvent", EWindowEvent::Resize, defaultResizeEvent, true);
+	InputManager::Get().AddWindowEventAction("DefaultExitMinimize", EWindowEvent::ExitMinimize, defaultResizeEvent, true);
+	InputManager::Get().AddWindowEventAction("DefaultEnterMaximize", EWindowEvent::EnterMaximize, defaultResizeEvent, true);
+	InputManager::Get().AddWindowEventAction("DefaultExitMaximize", EWindowEvent::ExitMaximize, defaultResizeEvent, true);
+
+	RenderManager::Get().SetVsyncMode(bIsVsync_);
+
+	bIsSetup_ = true;
+}
+
+void IApplication::Shutdown()
+{
+	if (bIsSetup_ && bIsPropertiesSet_)
+	{
+		ObjectManager::Get().Shutdown();
+		ResourceManager::Get().Shutdown();
+		RenderManager::Get().Shutdown();
+		AudioManager::Get().Shutdown();
+		InputManager::Get().Shutdown();
+
+		window_->Destroy();
+		window_.reset();
+
+		WindowClassUtils::UnregisterWindowClass();
+		WindowsCrashUtils::UnregisterWindowsExceptionFilter();
+
+		bIsSetup_ = false;
+		bIsPropertiesSet_ = false;
+	}
+}
+
+void IApplication::SetProperties(const std::wstring& windowTitle, int32_t windowPosX, int32_t windowPosY, int32_t windowWidth, int32_t windowHeight, bool bIsResize, bool bIsFullscreen, bool bIsVsync, bool bIsImGui)
+{
+	windowTitle_ = windowTitle;
+	windowPosition_ = Vector2i(windowPosX, windowPosY);
+	windowWidth_ = windowWidth;
+	windowHeight_ = windowHeight;
+	bIsResize_ = bIsResize;
+	bIsFullscreen_ = bIsFullscreen;
+	bIsVsync_ = bIsVsync;
+	bIsImGui_ = bIsImGui;
+
+	bIsPropertiesSet_ = true;
+}
