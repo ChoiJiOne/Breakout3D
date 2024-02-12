eaaaa6a96ce6fba50f4c93038737ba4612d4da87
DESKTOP-CIQEUKA\choij <choijione02@gmail.com>
2024-02-13 08:20:20 +0900

    SDL 라이브러리를 이용해서 윈도우를 생성하는 코드 추가

--- Breakout3D/Src/Main.cpp
+++ Breakout3D/Src/Main.cpp
@@ -6,6 +6,25 @@
 int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR pCmdLine, _In_ int32_t nCmdShow)
 {
 	SDL_Init(SDL_INIT_EVERYTHING);
+
+	SDL_Window* window = SDL_CreateWindow("Breakout3D", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1000, 800, SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL);
+
+	SDL_Event e;
+	bool bIsDone = false;
+	while (!bIsDone)
+	{
+		while (SDL_PollEvent(&e))
+		{
+			if (e.type == SDL_QUIT)
+			{
+				bIsDone = true;
+			}
+		}
+	}
+
+	SDL_DestroyWindow(window);
+	window = nullptr;
+
 	SDL_Quit();
 	return 0;
 }
