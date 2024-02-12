944db2771e12738f85ad72e2f9fa713c084b3ab4
DESKTOP-CIQEUKA\choij <choijione02@gmail.com>
2024-02-13 07:50:38 +0900

    SDL2 라이브러리가 올바르게 추가되었는지 확인하는 테스트 코드 추가

--- Breakout3D/Src/Main.cpp
+++ Breakout3D/Src/Main.cpp
@@ -1,7 +1,11 @@
 #include <cstdint>
 #include <windows.h>
 
+#include <SDL.h>
+
 int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR pCmdLine, _In_ int32_t nCmdShow)
 {
+	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_Quit();
 	return 0;
 }
