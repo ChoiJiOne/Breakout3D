c2edae223deed183d1b171e5060eafe03bcd7ab1
CHOIJIONE\choij <choijione02@gmail.com>
2024-02-13 14:09:31 +0900

    평가식을 검사하는 어선셜 매크로 추가

--- Breakout3D/Inc/Assertion.h
+++ Breakout3D/Inc/Assertion.h
@@ -57,3 +57,47 @@ inline void DebugPrintF(const wchar_t* format, ...)
 }
 #endif
 
+
+/**
+ * @brief 평가식을 검사합니다.
+ *
+ * @param Expression 검사할 호출값입니다.
+ * @param ... 평가식을 만족하지 못할 경우 표시할 가변 인자 메시지입니다.
+ *
+ * @note
+ * - Debug 모드와 Release 모드에서는 평가식을 검사하지만 Shipping 모드에서는 평가식을 검사하지 않습니다.
+ * - 디버거가 존재하면 브레이크 포인트가 걸립니다.
+ */
+#if defined(DEBUG_MODE)
+#ifndef ASSERT
+#define ASSERT(Expression, ...)\
+{\
+	if (!(bool)(Expression))                                                                                                                \
+	{                                                                                                                                       \
+		DebugPrintF("\nAssertion check point failed!\nFile : %s\nLine : %d\nExpression : %s\nMessage : ", __FILE__, __LINE__, #Expression); \
+		DebugPrintF(__VA_ARGS__);                                                                                                           \
+		DebugPrintF("\n");                                                                                                                  \
+		__debugbreak();                                                                                                                     \
+		ExitProcess(-1);                                                                                                                    \
+	}                                                                                                                                       \
+}
+#endif
+#elif defined(RELEASE_MODE) || defined(DEVELOPMENT_MODE)
+#ifndef ASSERT
+#define ASSERT(Expression, ...)\
+{\
+	if (!(bool)(Expression))                                                                                                                \
+	{                                                                                                                                       \
+		DebugPrintF("\nAssertion check point failed!\nFile : %s\nLine : %d\nExpression : %s\nMessage : ", __FILE__, __LINE__, #Expression); \
+		DebugPrintF(__VA_ARGS__);                                                                                                           \
+		DebugPrintF("\n");                                                                                                                  \
+		__debugbreak();                                                                                                                     \
+		ExitProcess(-1);                                                                                                                    \
+	}                                                                                                                                       \
+}
+#endif
+#else // defined(SHIPPING_MODE)
+#ifndef ASSERT
+#define ASSERT(Expression, ...) ((void)(Expression))
+#endif
+#endif
