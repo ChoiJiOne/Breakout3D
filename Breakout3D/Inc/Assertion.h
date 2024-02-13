63ae399db6513d617d3926ebfa97ccefd1da6371
CHOIJIONE\choij <choijione02@gmail.com>
2024-02-13 14:48:50 +0900

    OpenGL의 에러 코드에 대응하는 메시지를 얻는 기능 추가

--- Breakout3D/Inc/Assertion.h
+++ Breakout3D/Inc/Assertion.h
@@ -191,3 +191,127 @@ inline void DebugPrintF(const wchar_t* format, ...)
 #endif
 #endif
 #endif
+
+
+#if defined(__glad_h_)
+/**
+ * @brief 에러 코드에 대응하는 메시지를 얻습니다.
+ *
+ * @param errorCode 확인할 에러 코드입니다.
+ *
+ * @return 에러 코드에 대응하는 메시지를 반환합니다.
+ *
+ * @note
+ * - https://registry.khronos.org/OpenGL-Refpages/gl4/html/glGetError.xhtml
+ * - https://www.khronos.org/opengl/wiki/OpenGL_Error
+ */
+inline std::string GetGLErrorCodeMessageA(const GLenum & errorCode)
+{
+	std::string errorMessage;
+
+	switch (errorCode)
+	{
+	case GL_NO_ERROR:
+		errorMessage = "No error has been recorded. (GL_NO_ERROR)";
+		break;
+
+	case GL_INVALID_ENUM:
+		errorMessage = "An unacceptable value is specified for an enumerated argument. (GL_INVALID_ENUM)";
+		break;
+
+	case GL_INVALID_VALUE:
+		errorMessage = "A numeric argument is out of range. (GL_INVALID_VALUE)";
+		break;
+
+	case GL_INVALID_OPERATION:
+		errorMessage = "The specified operation is not allowed in the current state. (GL_INVALID_OPERATION)";
+		break;
+
+	case GL_INVALID_FRAMEBUFFER_OPERATION:
+		errorMessage = "The framebuffer object is not complete. (GL_INVALID_FRAMEBUFFER_OPERATION)";
+		break;
+
+	case GL_OUT_OF_MEMORY:
+		errorMessage = "There is not enough memory left to execute the command. (GL_OUT_OF_MEMORY)";
+		break;
+
+	case GL_STACK_UNDERFLOW:
+		errorMessage = "An attempt has been made to perform an operation that would cause an internal stack to underflow. (GL_STACK_UNDERFLOW)";
+		break;
+
+	case GL_STACK_OVERFLOW:
+		errorMessage = "An attempt has been made to perform an operation that would cause an internal stack to overflow. (GL_STACK_OVERFLOW)";
+		break;
+
+	case GL_CONTEXT_LOST:
+		errorMessage = "This occurs when the OpenGL context becomes lost due to a graphics card reset. (GL_CONTEXT_LOST)";
+		break;
+
+	default:
+		errorMessage = "Undefined error message.";
+	}
+
+	return errorMessage;
+}
+
+
+/**
+ * @brief 에러 코드에 대응하는 메시지를 얻습니다.
+ *
+ * @param errorCode 확인할 에러 코드입니다.
+ *
+ * @return 에러 코드에 대응하는 메시지를 반환합니다.
+ *
+ * @note
+ * - https://registry.khronos.org/OpenGL-Refpages/gl4/html/glGetError.xhtml
+ * - https://www.khronos.org/opengl/wiki/OpenGL_Error
+ */
+inline std::wstring GetGLErrorCodeMessageW(const GLenum & errorCode)
+{
+	std::wstring errorMessage;
+
+	switch (errorCode)
+	{
+	case GL_NO_ERROR:
+		errorMessage = L"No error has been recorded. (GL_NO_ERROR)";
+		break;
+
+	case GL_INVALID_ENUM:
+		errorMessage = L"An unacceptable value is specified for an enumerated argument. (GL_INVALID_ENUM)";
+		break;
+
+	case GL_INVALID_VALUE:
+		errorMessage = L"A numeric argument is out of range. (GL_INVALID_VALUE)";
+		break;
+
+	case GL_INVALID_OPERATION:
+		errorMessage = L"The specified operation is not allowed in the current state. (GL_INVALID_OPERATION)";
+		break;
+
+	case GL_INVALID_FRAMEBUFFER_OPERATION:
+		errorMessage = L"The framebuffer object is not complete. (GL_INVALID_FRAMEBUFFER_OPERATION)";
+		break;
+
+	case GL_OUT_OF_MEMORY:
+		errorMessage = L"There is not enough memory left to execute the command. (GL_OUT_OF_MEMORY)";
+		break;
+
+	case GL_STACK_UNDERFLOW:
+		errorMessage = L"An attempt has been made to perform an operation that would cause an internal stack to underflow. (GL_STACK_UNDERFLOW)";
+		break;
+
+	case GL_STACK_OVERFLOW:
+		errorMessage = L"An attempt has been made to perform an operation that would cause an internal stack to overflow. (GL_STACK_OVERFLOW)";
+		break;
+
+	case GL_CONTEXT_LOST:
+		errorMessage = L"This occurs when the OpenGL context becomes lost due to a graphics card reset. (GL_CONTEXT_LOST)";
+		break;
+
+	default:
+		errorMessage = L"Undefined error message.";
+	}
+
+	return errorMessage;
+}
+#endif
