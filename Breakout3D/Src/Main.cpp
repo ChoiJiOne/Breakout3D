e8998cff571a74c225ddce3f5259ac65c4ff62fe
DESKTOP-CIQEUKA\choij <choijione02@gmail.com>
2024-02-13 09:06:45 +0900

    OpenGL 컨텍스트 생성 및 백버퍼를 초기화하는 코드 추가

--- Breakout3D/Src/Main.cpp
+++ Breakout3D/Src/Main.cpp
@@ -1,13 +1,30 @@
 #include <cstdint>
 #include <windows.h>
 
+#include <glad/glad.h>
 #include <SDL.h>
 
 int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR pCmdLine, _In_ int32_t nCmdShow)
 {
 	SDL_Init(SDL_INIT_EVERYTHING);
 
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
+	SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+	SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+
 	SDL_Window* window = SDL_CreateWindow("Breakout3D", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1000, 800, SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL);
+	SDL_GLContext context = SDL_GL_CreateContext(window);
+
+	SDL_GL_MakeCurrent(window, context);
+	gladLoadGLLoader((GLADloadproc)(SDL_GL_GetProcAddress));
 
 	SDL_Event e;
 	bool bIsDone = false;
@@ -20,7 +37,15 @@ int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstan
 				bIsDone = true;
 			}
 		}
+
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		SDL_GL_SwapWindow(window);
 	}
+
+	SDL_GL_DeleteContext(context);
+	context = nullptr;
 
 	SDL_DestroyWindow(window);
 	window = nullptr;
