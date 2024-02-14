b7b2c2d8cd67b050163eaafe890bb5f405ff1e30
CHOIJIONE\choij <choijione02@gmail.com>
2024-02-14 11:02:52 +0900

    메인 함수의 API 호출 부분에 어선셜 매크로 추가

--- Breakout3D/Src/Main.cpp
+++ Breakout3D/Src/Main.cpp
@@ -4,27 +4,31 @@
 #include <glad/glad.h>
 #include <SDL.h>
 
+#include "Assertion.h"
+
 int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR pCmdLine, _In_ int32_t nCmdShow)
 {
-	SDL_Init(SDL_INIT_EVERYTHING);
-
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
-	SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
-	SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
-	SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
-	SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
-	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
-	SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
-	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+	SDL_FAILED(SDL_Init(SDL_INIT_EVERYTHING));
+	SDL_FAILED(SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG));
+	SDL_FAILED(SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE));
+	SDL_FAILED(SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4));
+	SDL_FAILED(SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6));
+	SDL_FAILED(SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8));
+	SDL_FAILED(SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8));
+	SDL_FAILED(SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8));
+	SDL_FAILED(SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8));
+	SDL_FAILED(SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24));
+	SDL_FAILED(SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8));
+	SDL_FAILED(SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1));
 
 	SDL_Window* window = SDL_CreateWindow("Breakout3D", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1000, 800, SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL);
-	SDL_GLContext context = SDL_GL_CreateContext(window);
+	SDL_ASSERT(window != nullptr, "failed to create window");
 
-	SDL_GL_MakeCurrent(window, context);
-	gladLoadGLLoader((GLADloadproc)(SDL_GL_GetProcAddress));
+	SDL_GLContext context = SDL_GL_CreateContext(window);
+	SDL_ASSERT(context != nullptr, "failed to create opengl context");
+
+	SDL_FAILED(SDL_GL_MakeCurrent(window, context));
+	CHECK(gladLoadGLLoader((GLADloadproc)(SDL_GL_GetProcAddress)));
 
 	SDL_Event e;
 	bool bIsDone = false;
