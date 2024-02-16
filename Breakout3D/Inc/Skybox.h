cdde8dc727c57e321fc2689c8800a0b582880473
DESKTOP-CIQEUKA\choij <choijione02@gmail.com>
2024-02-17 08:12:12 +0900

    큐브맵 텍스처 리소스를 로딩하여 스카이 박스를 생성하는 기능 추가

---
+++ Breakout3D/Inc/Skybox.h
@@ -0,0 +1,122 @@
+#pragma once
+
+#include <string>
+
+#include "IResource.h"
+
+
+/**
+ * @brief 큐브맵 이미지 파일들을 로딩하고 OpenGL 파이프라인에 바인딩 가능한 큐브맵 리소스를 생성합니다.
+ */
+class Skybox : public IResource
+{
+public:
+	/**
+	 * @brief 스카이 박스의 디폴트 생성자입니다.
+	 *
+	 * @note 생성자 이외의 메서드에서 적절한 초기화를 수행해야 합니다.
+	 */
+	Skybox() = default;
+
+
+	/**
+	 * @brief 스카이 박스의 가상 소멸자입니다.
+	 *
+	 * @note 큐브맵 내부에서 할당된 요소를 해제하기 위해서는 반드시 Release를 호출해야 합니다.
+	 */
+	virtual ~Skybox();
+
+
+	/**
+	 * @brief 스카이 박스의 복사 생성자와 대입 연산자를 명시적으로 삭제합니다.
+	 */
+	DISALLOW_COPY_AND_ASSIGN(Skybox);
+
+
+	/**
+	 * @brief 큐브맵 이미지 파일을 읽고 스카이 맵 리소스를 생성합니다.
+	 *
+	 * @param rightPath 큐브맵 RIGHT(+X) 부분의 텍스처 리소스 경로입니다.
+	 * @param leftPath 큐브맵 LEFT(-X) 부분의 텍스처 리소스 경로입니다.
+	 * @param topPath 큐브맵 TOP(+Y) 부분의 텍스처 리소스 경로입니다.
+	 * @param bottomPath 큐브맵 BOTTOM(-Y) 부분의 텍스처 리소스 경로입니다.
+	 * @param frontPath 큐브맵 FRONT(+Z) 부분의 텍스처 리소스 경로입니다.
+	 * @param backPath 큐브맵 BACK(-Z) 부분의 텍스처 리소스 경로입니다.
+	 */
+	void Initialize(
+		const std::string& rightPath,
+		const std::string& leftPath,
+		const std::string& topPath,
+		const std::string& bottomPath,
+		const std::string& frontPath,
+		const std::string& backPath
+	);
+
+
+	/**
+	 * @brief 스카이 박스 내부 리소스를 할당 해제합니다.
+	 */
+	virtual void Release() override;
+
+
+	/**
+	 * @brief 스카이 박스의 큐브맵을 활성화합니다.
+	 *
+	 * @param unit 활성화 할 텍스처 유닛입니다.
+	 */
+	void Active(uint32_t unit) const;
+
+
+	/**
+	 * @brief 스카이 박스의 정점 버퍼 오브젝트를 얻습니다.
+	 *
+	 * @return 스카이 박스의 정점 버퍼 오브젝트의 ID를 반환합니다.
+	 */
+	uint32_t GetVertexArrayObject() const { return vertexArrayObject_; }
+
+
+	/**
+	 * @brief 스카이 박스의 정점 버퍼의 수를 얻습니다.
+	 *
+	 * @return 스카이 박스의 정점 버퍼의 수를 반환합니다.
+	 */
+	uint32_t GetVertexCount() const { return vertexCount_; }
+
+
+private:
+	/**
+	 * @brief 파일로부터 픽셀 버퍼를 읽습니다.
+	 *
+	 * @param path 픽셀 버퍼 파일(이미지 파일)의 경로입니다.
+	 * @param outWidth 버퍼의 가로 크기입니다.
+	 * @param outHeight 버퍼의 세로 크기입니다.
+	 * @param outChannels 픽셀의 체널(1~4)입니다.
+	 * @param outPixelBuffer 픽셀 버퍼입니다.
+	 */
+	void ReadPixelBufferFromFile(const std::string& path, int32_t& outWidth, int32_t& outHeight, int32_t& outChannels, std::vector<uint8_t>& outPixelBuffer);
+
+
+private:
+	/**
+	 * @brief 큐브맵 텍스처의 ID입니다.
+	 */
+	uint32_t cubeMapID_ = 0;
+
+
+	/**
+	 * @brief 스카이 박스의 정점 목록의 오브젝트입니다.
+	 */
+	uint32_t vertexArrayObject_ = 0;
+
+
+	/**
+	 * @brief 스카이 박스의 정점 버퍼 오브젝트입니다.
+	 */
+	uint32_t vertexBufferObject_ = 0;
+
+
+	/**
+	 * @brief 스카이 박스 정점의 수입니다.
+	 */
+	uint32_t vertexCount_ = 0;
+};
---
+++ Breakout3D/Src/Skybox.cpp
@@ -0,0 +1,174 @@
+#include <vector>
+
+#include <glad/glad.h>
+#include <stb_image.h>
+
+#include "Assertion.h"
+#include "Skybox.h"
+
+#define INTERNAL_FORMAT_R    1
+#define INTERNAL_FORMAT_RG   2
+#define INTERNAL_FORMAT_RGB  3
+#define INTERNAL_FORMAT_RGBA 4
+
+Skybox::~Skybox()
+{
+	if (bIsInitialized_)
+	{
+		Release();
+	}
+}
+
+void Skybox::Initialize(const std::string& rightPath, const std::string& leftPath, const std::string& topPath, const std::string& bottomPath, const std::string& frontPath, const std::string& backPath)
+{
+	CHECK(!bIsInitialized_);
+
+	std::vector<std::string> resourcePaths = 
+	{
+		rightPath,
+		leftPath,
+		topPath,
+		bottomPath,
+		frontPath,
+		backPath,
+	};
+
+	GL_FAILED(glGenTextures(1, &cubeMapID_));
+	GL_FAILED(glBindTexture(GL_TEXTURE_CUBE_MAP, cubeMapID_));
+
+	for (std::size_t index = 0; index < resourcePaths.size(); ++index)
+	{
+		int32_t width = 0;
+		int32_t height = 0;
+		int32_t channels = 0;
+		std::vector<uint8_t> buffer;
+		ReadPixelBufferFromFile(resourcePaths[index], width, height, channels, buffer);
+
+		GLenum target = static_cast<GLenum>(GL_TEXTURE_CUBE_MAP_POSITIVE_X + index);
+		GLenum format = 0xFFFF;
+
+		switch (channels)
+		{
+		case INTERNAL_FORMAT_R:
+			format = GL_RED;
+			break;
+
+		case INTERNAL_FORMAT_RG:
+			format = GL_RG;
+			break;
+
+		case INTERNAL_FORMAT_RGB:
+			format = GL_RGB;
+			break;
+
+		case INTERNAL_FORMAT_RGBA:
+			format = GL_RGBA;
+			break;
+
+		default:
+			ASSERT(false, "undefined format");
+			break;
+		}
+
+		GL_FAILED(glTexImage2D(target, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, buffer.data()));
+	}
+
+	GL_FAILED(glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR));
+	GL_FAILED(glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR));
+	GL_FAILED(glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE));
+	GL_FAILED(glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE));
+	GL_FAILED(glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE));
+
+	GL_FAILED(glBindTexture(GL_TEXTURE_CUBE_MAP, 0));
+
+
+	std::vector<float> vertices = 
+	{
+		-1.0f, +1.0f, -1.0f,
+		-1.0f, -1.0f, -1.0f,
+		+1.0f, -1.0f, -1.0f,
+		+1.0f, -1.0f, -1.0f,
+		+1.0f, +1.0f, -1.0f,
+		-1.0f, +1.0f, -1.0f,
+
+		-1.0f, -1.0f,  1.0f,
+		-1.0f, -1.0f, -1.0f,
+		-1.0f, +1.0f, -1.0f,
+		-1.0f, +1.0f, -1.0f,
+		-1.0f, +1.0f, +1.0f,
+		-1.0f, -1.0f, +1.0f,
+
+		+1.0f, -1.0f, -1.0f,
+		+1.0f, -1.0f, +1.0f,
+		+1.0f, +1.0f, +1.0f,
+		+1.0f, +1.0f, +1.0f,
+		+1.0f, +1.0f, -1.0f,
+		+1.0f, -1.0f, -1.0f,
+
+		-1.0f, -1.0f,  1.0f,
+		-1.0f,  1.0f,  1.0f,
+		+1.0f,  1.0f,  1.0f,
+		+1.0f,  1.0f,  1.0f,
+		+1.0f, -1.0f,  1.0f,
+		-1.0f, -1.0f,  1.0f,
+
+		-1.0f,  1.0f, -1.0f,
+		+1.0f,  1.0f, -1.0f,
+		+1.0f,  1.0f,  1.0f,
+		+1.0f,  1.0f,  1.0f,
+		-1.0f,  1.0f,  1.0f,
+		-1.0f,  1.0f, -1.0f,
+
+		-1.0f, -1.0f, -1.0f,
+		-1.0f, -1.0f,  1.0f,
+		+1.0f, -1.0f, -1.0f,
+		+1.0f, -1.0f, -1.0f,
+		-1.0f, -1.0f,  1.0f,
+		+1.0f, -1.0f,  1.0f
+	};
+	vertexCount_ = static_cast<uint32_t>(vertices.size());
+
+	GL_FAILED(glGenVertexArrays(1, &vertexArrayObject_));
+	GL_FAILED(glGenBuffers(1, &vertexBufferObject_));
+	GL_FAILED(glBindVertexArray(vertexArrayObject_));
+	GL_FAILED(glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObject_));
+	GL_FAILED(glBufferData(GL_ARRAY_BUFFER, sizeof(float) * static_cast<uint32_t>(vertices.size()), vertices.data(), GL_STATIC_DRAW));
+
+	GL_FAILED(glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)(0)));
+	GL_FAILED(glEnableVertexAttribArray(0));
+
+	GL_FAILED(glBindVertexArray(0));
+
+	bIsInitialized_ = true;
+}
+
+void Skybox::Release()
+{
+	CHECK(bIsInitialized_);
+
+	GL_FAILED(glDeleteTextures(1, &cubeMapID_));
+	GL_FAILED(glDeleteBuffers(1, &vertexBufferObject_));
+	GL_FAILED(glDeleteVertexArrays(1, &vertexArrayObject_));
+
+	bIsInitialized_ = false;
+}
+
+void Skybox::Active(uint32_t unit) const
+{
+	GL_FAILED(glActiveTexture(GL_TEXTURE0 + unit));
+	GL_FAILED(glBindTexture(GL_TEXTURE_CUBE_MAP, cubeMapID_));
+}
+
+void Skybox::ReadPixelBufferFromFile(const std::string& path, int32_t& outWidth, int32_t& outHeight, int32_t& outChannels, std::vector<uint8_t>& outPixelBuffer)
+{
+	uint8_t* bufferPtr = stbi_load(path.c_str(), &outWidth, &outHeight, &outChannels, 0);
+	ASSERT(bufferPtr != nullptr, "failed to load %s file", path.c_str());
+
+	std::size_t bufferSize = static_cast<std::size_t>(outWidth * outHeight * outChannels);
+	outPixelBuffer.resize(bufferSize);
+
+	std::copy(bufferPtr, bufferPtr + bufferSize, outPixelBuffer.data());
+
+	stbi_image_free(bufferPtr);
+	bufferPtr = nullptr;
+}
