dcb07995ce5c8c2911031c870df103afe190b206
CHOIJIONE\choij <choijione02@gmail.com>
2024-02-20 13:53:31 +0900

    그림자 매핑(Shadow Mapping) 구현을 위한 씬 세팅 추가
    - 기존의 코드 정리
    - 리소스 로딩 부분 함수로 정리
    - 일부 변수들 전역 변수로 분리

--- Breakout3D/Src/Main.cpp
+++ Breakout3D/Src/Main.cpp
@@ -4,9 +4,6 @@
 
 #include <glad/glad.h>
 #include <SDL.h>
-#include <imgui.h>
-#include <imgui_impl_sdl2.h>
-#include <imgui_impl_opengl3.h>
 
 #include "CrashModule.h"
 #include "FileModule.h"
@@ -24,6 +21,53 @@
 #include "Texture2D.h"
 #include "Vertex.h"
 
+StaticMesh* sphere = nullptr;
+StaticMesh* cube = nullptr;
+
+SkyboxPass* skyboxPass = nullptr;
+Shader* lightPass = nullptr;
+Shader* visualLightPass = nullptr;
+
+Texture2D* texture = nullptr;
+Skybox* skybox = nullptr;
+
+Vec3f viewPosition = Vec3f(10.0f, 10.0f, 10.0f);
+Vec3f lightPosition = Vec3f(0.0f, 3.0f, 0.0f);
+Vec4f lightColor = Vec4f(1.0f, 1.0f, 1.0f, 1.0f);
+
+void Load()
+{
+	std::vector<Vertex> vertices;
+	std::vector<uint32_t> indices;
+
+	GeometryGenerator::CreateSphere(1.0f, 40, vertices, indices);
+	sphere = ResourceManager::Get().CreateResource<StaticMesh>("Sphere");
+	sphere->Initialize(vertices, indices);
+
+	GeometryGenerator::CreateCube(Vec3f(10.0f, 1.0f, 10.0f), vertices, indices);
+	cube = ResourceManager::Get().CreateResource<StaticMesh>("Cube");
+	cube->Initialize(vertices, indices);
+
+	lightPass = ResourceManager::Get().CreateResource<Shader>("Shader");
+	lightPass->Initialize("Shader/Shader.vert", "Shader/Shader.frag");
+
+	skyboxPass = ResourceManager::Get().CreateResource<SkyboxPass>("SkyboxPass");
+	skyboxPass->Initialize();
+
+	visualLightPass = ResourceManager::Get().CreateResource<Shader>("VisualLight");
+	visualLightPass->Initialize("Shader/VisualLight.vert", "Shader/VisualLight.frag");
+
+	texture = ResourceManager::Get().CreateResource<Texture2D>("Texture");
+	texture->Initialize("Resource/earth.png");
+
+	skybox = ResourceManager::Get().CreateResource<Skybox>("Skybox");
+	skybox->Initialize(
+		"Resource/Skybox/Space_Right.png", "Resource/Skybox/Space_Left.png",
+		"Resource/Skybox/Space_Top.png", "Resource/Skybox/Space_Bottom.png",
+		"Resource/Skybox/Space_Front.png", "Resource/Skybox/Space_Back.png"
+	);
+}
+
 int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR pCmdLine, _In_ int32_t nCmdShow)
 {
 	CHECK(CrashModule::RegisterExceptionFilter());
@@ -35,50 +79,8 @@ int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstan
 	RenderManager::Get().SetDepthMode(true);
 	RenderManager::Get().SetAlphaBlendMode(true);
 	RenderManager::Get().SetMultisampleMode(true);
-
-	ImGui::CreateContext();
-	ImGuiIO& io = ImGui::GetIO();
-	io.IniFilename = nullptr;
-
-	ImGui::StyleColorsDark();
-
-	SDL_Window* window = reinterpret_cast<SDL_Window*>(SDLManager::Get().GetMainWindow());
-
-	ImGui_ImplSDL2_InitForOpenGL(window, SDL_GL_GetCurrentContext());
-	ImGui_ImplOpenGL3_Init("#version 460 core");
-
-	std::vector<Vertex> vertices;
-	std::vector<uint32_t> indices;
-	GeometryGenerator::CreateSphere(1.0f, 40, vertices, indices);
 	
-	Shader* lightPass = ResourceManager::Get().CreateResource<Shader>("Shader");
-	lightPass->Initialize("Shader/Shader.vert", "Shader/Shader.frag");
-
-	SkyboxPass* skyboxPass = ResourceManager::Get().CreateResource<SkyboxPass>("SkyboxPass");
-	skyboxPass->Initialize();
-
-	Shader* visualLightPass = ResourceManager::Get().CreateResource<Shader>("VisualLight");
-	visualLightPass->Initialize("Shader/VisualLight.vert", "Shader/VisualLight.frag");
-
-	Texture2D* texture = ResourceManager::Get().CreateResource<Texture2D>("Texture");
-	texture->Initialize("Resource/earth.png");
-
-	Skybox* skybox = ResourceManager::Get().CreateResource<Skybox>("Skybox");
-	skybox->Initialize(
-		"Resource/Skybox/Space_Right.png",
-		"Resource/Skybox/Space_Left.png",
-		"Resource/Skybox/Space_Top.png",
-		"Resource/Skybox/Space_Bottom.png",
-		"Resource/Skybox/Space_Front.png",
-		"Resource/Skybox/Space_Back.png"
-	);
-
-	StaticMesh* mesh = ResourceManager::Get().CreateResource<StaticMesh>("StaticMesh");
-	mesh->Initialize(vertices, indices);
-
-	Vec3f viewPosition = Vec3f(5.0f, 5.0f, 5.0f);
-	Vec3f lightPosition = Vec3f(0.0f, 3.0f, 0.0f);
-	Vec4f lightColor = Vec4f(1.0f, 1.0f, 1.0f, 1.0f);
+	Load();
 
 	SDL_Event e;
 	bool bIsDone = false;
@@ -86,7 +88,6 @@ int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstan
 	{
 		while (SDL_PollEvent(&e))
 		{
-			ImGui_ImplSDL2_ProcessEvent(&e);
 			switch (e.type)
 			{
 			case SDL_QUIT:
@@ -95,57 +96,55 @@ int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstan
 			}
 		}
 
-		ImGui_ImplOpenGL3_NewFrame();
-		ImGui_ImplSDL2_NewFrame();
-		ImGui::NewFrame();
-
-		{
-			ImGui::Begin("Light Position");
-			ImGui::SliderFloat("x", &lightPosition.x, -10.0f, 10.0f);
-			ImGui::SliderFloat("z", &lightPosition.z, -10.0f, 10.0f);
-			ImGui::End();
-		}
-
 		RenderManager::Get().BeginFrame(0.0f, 0.0f, 0.0f, 1.0f);
 
-		Mat4x4f world = MathModule::CreateScale(Vec3f(0.3f, 0.3f, 0.3f)) * MathModule::CreateTranslation(lightPosition);
+		Mat4x4f world = MathModule::CreateScale(Vec3f(0.2f, 0.2f, 0.2f)) * MathModule::CreateTranslation(lightPosition);
 		Mat4x4f view = MathModule::CreateLookAt(viewPosition, Vec3f(0.0f, 0.0f, 0.0f), Vec3f(0.0f, 1.0f, 0.0f));
 		Mat4x4f projection = MathModule::CreatePerspective(MathModule::ToRadian(45.0f), static_cast<float>(1000) / static_cast<float>(800), 0.1f, 100.0f);
 
-		{
-			skyboxPass->Draw(view, projection, skybox);
-			
+		skyboxPass->Draw(view, projection, skybox);
+
+		{ // 1. 씬 그리기
 			texture->Active(0);
+
 			lightPass->Bind();
-			lightPass->SetUniform("world", Mat4x4f::Identity());
 			lightPass->SetUniform("view", view);
 			lightPass->SetUniform("projection", projection);
 			lightPass->SetUniform("lightPosition", lightPosition);
 			lightPass->SetUniform("viewPosition", viewPosition);
-			mesh->Bind();
-			glDrawElements(GL_TRIANGLES, mesh->GetIndexCount(), GL_UNSIGNED_INT, 0);
-			mesh->Unbind();
-			lightPass->Unbind();
 
+			sphere->Bind();
+
+			lightPass->SetUniform("world", MathModule::CreateTranslation(-2.0f, 1.0f, 0.0f));
+			glDrawElements(GL_TRIANGLES, sphere->GetIndexCount(), GL_UNSIGNED_INT, 0);
+
+			lightPass->SetUniform("world", MathModule::CreateTranslation(+2.0f, 1.0f, 0.0f));
+			glDrawElements(GL_TRIANGLES, sphere->GetIndexCount(), GL_UNSIGNED_INT, 0);
+
+			sphere->Unbind();
+
+			cube->Bind();
+			lightPass->SetUniform("world", MathModule::CreateTranslation(Vec3f(0.0f, -0.5f, 0.0f)));
+			glDrawElements(GL_TRIANGLES, cube->GetIndexCount(), GL_UNSIGNED_INT, 0);
+			cube->Unbind();
+
+			lightPass->Unbind();
+		}
+
+		{ // 2. 화면에 라이트 그리기
 			visualLightPass->Bind();
 			visualLightPass->SetUniform("world", world);
 			visualLightPass->SetUniform("view", view);
 			visualLightPass->SetUniform("projection", projection);
 			visualLightPass->SetUniform("lightColor", lightColor);
-			mesh->Bind();
-			glDrawElements(GL_TRIANGLES, mesh->GetIndexCount(), GL_UNSIGNED_INT, 0);
-			mesh->Unbind();
+			sphere->Bind();
+			glDrawElements(GL_TRIANGLES, sphere->GetIndexCount(), GL_UNSIGNED_INT, 0);
+			sphere->Unbind();
 			visualLightPass->Unbind();
 		}
 
-		ImGui::Render();
-		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 		RenderManager::Get().EndFrame();
 	}
-
-	ImGui_ImplOpenGL3_Shutdown();
-	ImGui_ImplSDL2_Shutdown();
-	ImGui::DestroyContext();
 
 	ResourceManager::Get().Shutdown();
 	RenderManager::Get().Shutdown();
